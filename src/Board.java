85a17ed9713184bb7cd34dcc9805cb04515b9704
xXFROSTBYTEXx <berniececchini@live.com>
2019-03-26 10:49:36 -0400

    Implemented Jack's 'random' board constructor. Works, but the pieces need to be placed on the right spaces on boards larger than 8x8

--- src/Board.java
+++ src/Board.java
@@ -2,6 +2,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Random;
 
 /**
  * Stores a chess board as an object, and provides helper methods.
@@ -17,6 +18,7 @@ public class Board {
     private Space[][] b;// 2D array that represents board.
     private int rows;
     private int cols;
+//    private boolean[][] spaceNulls;
 
     /**
      * Default constructor. Makes a normal 8x8 board.
@@ -86,6 +88,120 @@ public class Board {
             }
         }
     }
+
+    public Board(int size, boolean isRandom) {
+        if (isRandom) {
+//            spaceNulls = new boolean[size][size];
+            b = new Space[size][size];
+            rows = size;
+            cols = size;
+            Random randomNum = new Random();
+            int row = 0;
+            int column = 0;
+
+            for (row = 4; row < 7; row++) {
+                for (column = 4; column <= 11; column++) {
+                    b[row][column] = new Space();
+                }
+            }
+            for (row = 9; row < 12; row++) {
+                for (column = 4; column <= 11; column++) {
+                    b[row][column] = new Space();
+                }
+            }
+
+            //Randomize middle part of board
+            int i = 0;
+            if (8 > randomNum.nextInt(10)) {
+                createTile(4, 7, 1, 2);
+                createTile(11, 7, 1, 2);
+                i++;
+            }
+            if (8 > randomNum.nextInt(10)) {
+                createTile(5, 7, 1, 2);
+                createTile(10, 7, 1, 2);
+                i++;
+            }
+            if (8 > randomNum.nextInt(10)) {
+                createTile(6, 7, 1, 2);
+                createTile(9, 7, 1, 2);
+                i++;
+            }
+            if (8 > randomNum.nextInt(10)) {
+                createTile(7, 7, 1, 2);
+                createTile(8, 7, 1, 2);
+                i++;
+            }
+            if (i == 0) {
+                createTile(6, 7, 1, 2);
+                createTile(9, 7, 1, 2);
+                createTile(7, 7, 1, 2);
+                createTile(8, 7, 1, 2);
+            }
+            //randomizing corner parts
+            int rand = randomNum.nextInt(3);
+            if (0 == rand) {
+                createTile(1, 1, 4, 6);
+                createTile(11, 1, 4, 6);
+                createTile(1, 9, 4, 6);
+                createTile(11, 9, 4, 6);
+            } else if (1 == rand) {
+                createTile(2, 2, 3, 5);
+                createTile(11, 2, 3, 5);
+                createTile(2, 9, 3, 5);
+                createTile(11, 9, 3, 5);
+            } else {
+                createTile(5, 3, 2, 4);
+                createTile(9, 3, 2, 4);
+                createTile(5, 9, 2, 4);
+                createTile(9, 9, 2, 4);
+            }
+            //randomizing side parts
+            int rand2 = randomNum.nextInt(4);
+            if (rand2 == 0) {
+                createTile(1, 6, 3, 4);
+                createTile(12, 6, 3, 4);
+            }
+            if (rand2 == 1 || rand2 == 2) {
+                createTile(2, 6, 2, 4);
+                createTile(12, 6, 2, 4);
+            }
+            if (rand2 == 2 || rand2 == 1 || rand == 2) {
+                createTile(3, 6, 1, 4);
+                createTile(12, 6, 1, 4);
+            }
+            if (0 == randomNum.nextInt(2)) {
+                createTile(6, 12, 4, 2);
+                createTile(6, 2, 4, 2);
+            }
+            if (0 == randomNum.nextInt(2)) {
+                createTile(6, 12, 4, 1);
+                createTile(6, 3, 4, 1);
+            }
+            if (0 == randomNum.nextInt(2)) {
+                createTile(4, 3, 7, 1);
+                createTile(4, 12, 7, 1);
+            }
+
+//            b = new Space[size][size];
+//            for (int a = 0; a < size; a++) {
+//                for (int c = 0; c < size; c++) {
+//                    if (spaceNulls[a][c]) {
+//                        b[a][c] = new Space();
+//                    } else {
+//                        b[a][c] = null;
+//                    }
+//                }
+//            }
+        }
+    }
+
+    private void createTile(int startColumn, int startRow, int width, int height) {
+        for (int m_startRow = startRow; m_startRow < height + startRow; m_startRow++)
+            for (int m_startColumn = startColumn; m_startColumn < width + startColumn; m_startColumn++)
+                b[m_startRow][m_startColumn] = new Space();
+    }
+
 
     /**
      * Returns the explicit array that Board implies.
