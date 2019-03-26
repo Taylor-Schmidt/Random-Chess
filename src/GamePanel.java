847b7872435bac7c288e9994f48ebb813438baee
Ryan <byrnesr@sunypoly.edu>
2019-03-26 19:57:28 -0400

    Put pieces on the board.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -23,8 +23,48 @@ public class GamePanel extends JPanel {
         height = h;
 
         //Testing to see if icons for a piece will be displayed correctly.
-        Space space = new Space(new Queen("black"));
-        board.setSpace(space, 0, 0);
+        Space space;
+        space= new Space(new Rook("black"));
+        board.setSpace(space, 4, 4);
+        space= new Space(new Knight("black"));
+        board.setSpace(space, 4, 5);
+        space= new Space(new Bishop("black"));
+        board.setSpace(space, 4, 6);
+        space= new Space(new Queen("black"));
+        board.setSpace(space, 4, 7);
+        space= new Space(new King("black"));
+        board.setSpace(space, 4, 8);
+        space= new Space(new Bishop("black"));
+        board.setSpace(space, 4, 9);
+        space= new Space(new Knight("black"));
+        board.setSpace(space, 4, 10);
+        space= new Space(new Rook("black"));
+        board.setSpace(space, 4, 11);
+        for(int i=4; i<12; i++) {
+            space= new Space(new Pawn("black"));
+            board.setSpace(space, 5, i);
+        }
+
+        space= new Space(new Rook("white"));
+        board.setSpace(space, 11, 4);
+        space= new Space(new Knight("white"));
+        board.setSpace(space, 11, 5);
+        space= new Space(new Bishop("white"));
+        board.setSpace(space, 11, 6);
+        space= new Space(new King("white"));
+        board.setSpace(space, 11, 7);
+        space= new Space(new Queen("white"));
+        board.setSpace(space, 11, 8);
+        space= new Space(new Bishop("white"));
+        board.setSpace(space, 11, 9);
+        space= new Space(new Knight("white"));
+        board.setSpace(space, 11, 10);
+        space= new Space(new Rook("white"));
+        board.setSpace(space, 11, 11);
+        for(int i=4; i<12; i++) {
+            space= new Space(new Pawn("white"));
+            board.setSpace(space, 10, i);
+        }
 
         boardPanel = new BoardPanel(w, h, board);
         setLayout(new GridBagLayout());
